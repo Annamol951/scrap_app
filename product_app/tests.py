2e1713e3ca2cc081781ab0d41614fff5f66e3f51
Annamol951 <101315600+Annamol951@users.noreply.github.com>
2023-09-21 12:22:43 +0530

    Delete product_app directory

--- product_app/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> dba2744af0211525797b719d8ac9cde5b19f08b7
